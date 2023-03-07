096542bef8d7947418c728d8e5a89b4dd0b4f9c3
v11run <appalasaisuryavarun@gmail.com>
2023-03-07 10:20:36 +0530

    init setup done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+    colors:{
+      'green': '#006400',
+      'white': '#d3d3d3',
+    },
+  plugins: [],
+}
